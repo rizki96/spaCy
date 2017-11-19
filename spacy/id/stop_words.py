f80e3674c4ca70da0a5af84a53e493b341f65a9b
Iskandar Rizki <iskandar.rizki@detik.com>
2017-11-19 08:48:36 +0700

    sync from v1.9 id

---
+++ spacy/id/stop_words.py
@@ -0,0 +1,8 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+
+# stop words as whitespace-separated list
+STOP_WORDS = set("""
+saya aku gue kamu kau dia ia kami kalian beliau elo lo loe lu elu ini itu mereka yang apa kapan kemana dimana kenapa mengapa siapa bagaimana gimana telah suatu karena dan atau tapi tetapi dari ke di hingga selama pun sehingga untuk dengan antara sebelum sesudah lagi maka jika sekali semua lebih kurang paling beberapa bukan hanya bila sangat sama harus sekarang kemarin yaitu seperti bak yg dalam pada oleh sebab memang tak tidak ya iya emang berapa begitu juga begini gini gitu sana sini yakni misalnya sendiri justru tersebut merupakan adalah amat terlalu sekian demikian menjadi kepada makin semakin kata ujar bagai bagaikan melainkan padahal sedang sedangkan sekedar sekadar & ketika namun bisa saja beserta telanjur belum sudah sempat akhirnya nyaris atas
+""".split())
---
+++ spacy/id/tokenizer_exceptions.py
@@ -0,0 +1,53 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+from ..symbols import *
+from ..language_data import PRON_LEMMA
+
+
+"""
+TOKENIZER_EXCEPTIONS = {
+    "Jan.": [
+        {ORTH: "Jan.", LEMMA: "January"}
+    ]
+}
+"""
+
+TOKENIZER_EXCEPTIONS = {}
+
+# exceptions mapped to a single token containing only ORTH property
+# example: {"string": [{ORTH: "string"}]}
+# converted using strings_to_exc() util
+
+"""
+ORTH_ONLY = [
+    "a.",
+    "b.",
+    "c.",
+    "d.",
+    "e.",
+    "f.",
+    "g.",
+    "h.",
+    "i.",
+    "j.",
+    "k.",
+    "l.",
+    "m.",
+    "n.",
+    "o.",
+    "p.",
+    "q.",
+    "r.",
+    "s.",
+    "t.",
+    "u.",
+    "v.",
+    "w.",
+    "x.",
+    "y.",
+    "z."
+]
+"""
+
+ORTH_ONLY = []
