0b0582ab48ce0c3f33e99f83cde40fa51fde5120
Iskandar Rizki <iskandar.rizki@bhinneka.com>
2018-05-04 14:57:51 +0700

    adding tag_map

--- spacy/lang/id/__init__.py
+++ spacy/lang/id/__init__.py
@@ -5,6 +5,7 @@ from .stop_words import STOP_WORDS
 from .punctuation import TOKENIZER_SUFFIXES, TOKENIZER_PREFIXES, TOKENIZER_INFIXES
 from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .norm_exceptions import NORM_EXCEPTIONS
+from .tag_map import TAG_MAP
 from .lemmatizer import LOOKUP
 from .lex_attrs import LEX_ATTRS
 from .syntax_iterators import SYNTAX_ITERATORS
@@ -20,16 +21,19 @@ def _return_id(_):
 
 class BahasaDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
-    lex_attr_getters[LANG] = lambda text: 'id'
     lex_attr_getters.update(LEX_ATTRS)
+    lex_attr_getters[LANG] = _return_id
+    lex_attr_getters[NORM] = add_lookups(Language.Defaults.lex_attr_getters[NORM],
+                                         BASE_NORMS, NORM_EXCEPTIONS)
     tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
+
+    tag_map = TAG_MAP
     stop_words = STOP_WORDS
     prefixes = TOKENIZER_PREFIXES
     suffixes = TOKENIZER_SUFFIXES
     infixes = TOKENIZER_INFIXES
     syntax_iterators = SYNTAX_ITERATORS
     lemma_lookup = LOOKUP
-
 
 class Bahasa(Language):
     lang = 'id'
