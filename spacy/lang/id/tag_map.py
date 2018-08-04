87d185a8704121cfcea981c1a6aad2243e20e5c9
Iskandar Rizki <iskandar.rizki@bhinneka.com>
2018-08-04 21:25:06 +0700

    Fix bug in lang id

--- spacy/lang/id/tag_map.py
+++ spacy/lang/id/tag_map.py
@@ -1,7 +1,7 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-from ...symbols import POS, PUNCT, SYM, ADJ, CCONJ, NUM, DET, ADV, ADP, X, VERB
+from ...symbols import POS, PUNCT, SYM, ADJ, CONJ, SCONJ, NUM, DET, ADV, ADP, X, VERB
 from ...symbols import NOUN, PROPN, PART, INTJ, SPACE, PRON
 
 """
